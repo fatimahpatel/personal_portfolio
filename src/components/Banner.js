8f0e268e1fc62fb08827367239a012c56e3d8cd6
Fatimah Patel <fatimahpatel10@gmail.com>
2023-03-24 19:52:19 +0000

    changed /svg to .svg in Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import {useState, useEffect} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img/svg";
+import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
 
