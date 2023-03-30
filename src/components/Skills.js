276fa98bfb81d403ff89125f0b38967c659b72a0
Fatimah Patel <fatimahpatel10@gmail.com>
2023-03-30 22:11:15 +0100

    Added styling to the Skill section, resizing and formatting

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -39,7 +39,7 @@ export const Skills = () => {
                 Skills
               </h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-              <Carousel responsive={responsive} infinite={true} className="skills-slider">
+              <Carousel responsive={responsive} infinite={true} className="skill-slider">
                 <div className="item">
                   <img src={meter1} alt="Image"/>
                   <h5>Web Development</h5>
