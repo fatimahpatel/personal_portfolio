14201d3abd08b33e427b866c673ce96647c24333
Fatimah Patel <fatimahpatel10@gmail.com>
2023-04-03 21:56:23 +0100

    created buttonText and status useStates to be updated when submitting form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,6 +11,11 @@ export const Contact = () => {
     }
     //need states to store fields for contact and reset it later on 
     const [formDetails, setFormDetails] = useState(formInitialDetails);
+    //submit button initially set to 'send'
+    //when user presses send, it would update to 'sending'
+    const [buttonText, setButtonText] = useState('Send');
+    //status is for when we make the api call afterwards, whether or not we get error message or message has been sent successfully
+    const[status, setStatus] = useState({});
 
     
 }
