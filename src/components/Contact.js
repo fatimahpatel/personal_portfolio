068d7335e8732568138f7fb2623c9f497f0a17da
Fatimah Patel <fatimahpatel10@gmail.com>
2023-04-06 22:10:52 +0100

    added fields for email address and phone number for user to add details

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -44,6 +44,12 @@ export const Contact = () => {
                                 <Col sm={6} className="px-1">
                                     <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e)=>onFormUpdate('lastName', e.target.value)}/>
                                 </Col>
+                                <Col sm={6} className="px-1">
+                                    <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e)=>onFormUpdate('email', e.target.value)}/>
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e)=>onFormUpdate('phone', e.target.value)}/>
+                                </Col>
                             </Row>
                         </form>
                     </Col>
