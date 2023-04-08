e3ae3df784bddb18611a83ddad10fc2093df6663
Fatimah Patel <fatimahpatel10@gmail.com>
2023-04-08 21:27:08 +0100

    added nodemailer post request code and handleSubmit function

---
+++ server.js
@@ -0,0 +1,57 @@
+//declare all the modules we just installed
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+//set up a server and which port it runs in (port 5000)
+//run using npm server
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+
+//set up a way to send email
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: ''
+      pass: ''
+    },
+  });
+
+  contactEmail.verify((error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Ready to Send");
+    }
+  });
+  
+  //post request
+  router.post("/contact", (req, res) => {
+    const name = req.body.firstName + req.body.lastName;
+    const email = req.body.email;
+    const message = req.body.message;
+    const phone = req.body.phone;
+    const mail = {
+      from: name,
+      to: "********@gmail.com",
+      subject: "Contact Form Submission - Portfolio",
+      html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Phone: ${phone}</p>
+             <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+      if (error) {
+        res.json(error);
+      } else {
+        res.json({ code: 200, status: "Message Sent" });
+      }
+    });
+  });
