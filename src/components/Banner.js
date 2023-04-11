a9e6e94334c1508951dc7f5bd190f4832f03d24d
Fatimah Patel <fatimahpatel10@gmail.com>
2023-04-11 21:28:41 +0100

    added fade effect to Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,8 @@ import {useState, useEffect} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
 
@@ -63,11 +65,17 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
+                        
+                        <TrackVisibility>
+                        {/* destructured isVisible */}
+                        {({isVisible}) =>
+                        <div className={isVisible ? "animate__animated animate__fadeIn":""}>
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1>{`Hi I'm Fatimah `}<span className="wrap">{text}</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         <button onClick={() => console.log('connect')}>Let's Connect! <ArrowRightCircle size={25}/></button>
-
+                        </div>}
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header image"/>
